66197f4bbb8e131b6533cfeeab4d0e556a878c20
xiangxingx <xiangxingx@gmail.com>
2019-05-31 15:04:24 +0800

    添加功能

--- main.js
+++ main.js
@@ -11,16 +11,23 @@ document.addEventListener('click', function () {
   console.log(2)
 }) */
 
+let target = true
 $(btn).on('click', function () {
-  $(popover).show() // jQuery 高版本不能用show？
-  console.log(1)
+  if (target) {
+    $(popover).show() // jQuery 高版本不能用show？
+  } else {
+    $(popover).hide()
+  }
   $(document).one('click', function () { // 只监听一次，且在btn被点击时
     $(popover).hide() // hide()
-    console.log(3)
+    target = !target
+    console.log('document',target)
   })
+  target = !target
+  console.log('btn',target)
 })
+
 $(wrapper).on('click', function (ev) {
   ev.stopPropagation()
-  console.log(2)
 })
 
